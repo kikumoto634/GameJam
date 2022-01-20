3707ce2e5c3065a1fe948b1b8c9b32eec32f73f8
634_3 <k020g1146@g.neec.ac.jp>
2022-01-20 18:28:19 +0900

    playerをカメラから見て正面方向に飛ばす

--- Assets/EraserControl.cs
+++ Assets/EraserControl.cs
@@ -1,17 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EraserControl : MonoBehaviour
 {
+    [SerializeField] private float ForcePower = 10f;
+    private float _power;
+    public bool IsChange = false;
+
     public Rigidbody _rb = null;
 
-    [SerializeField] private float ForcePower = 10f;
+    public Slider _slider = null;
+
+
+    private void Start()
+    {
+        _power = 0;
+        IsChange = false;
+        _slider.value = 0;
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButton(0))
         {
-            _rb.AddForce(transform.forward * ForcePower, ForceMode.Impulse);
+            if(!IsChange && _power < ForcePower)
+            {
+                _power += 0.1f;
+                if(_power >= ForcePower) IsChange = true;
+            }
+            else if(IsChange && _power > 0.0f)
+            {
+                _power -= 0.1f;
+                if(_power <= 0.0f) IsChange = false;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {   
+            Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+            _rb.AddForce(cameraForward * (_power*10), ForceMode.Impulse);
+            _rb.AddTorque(Vector3.up * Mathf.PI * (_power*10), ForceMode.Force);
+            _power = 0;
+            IsChange = false;
             Debug.Log("”­ŽË");
         }
+
+        Debug.Log(_power);
+        //0.1‚Å‘¬“x§ŒÀ
+        _slider.value = _power * 0.1f;
     }
 }
